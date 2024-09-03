17b0875624e4631dbf303acc1511bc8b76f4fcb5
ArmanAbadi <arman_abadi@hotmail.com>
2024-09-03 13:53:25 -0700

    Try to make navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 import Link from "next/link";
+import NavBar from "./components/NavBar";
+import imagePath from "./assets/your_image_icon.png";
 
 const Home = () => {
+    let items = ["Home", "Product", "Service"];
+
   return (
-    <div>
+   <div>
+          <NavBar
+          brandName="My Brand"
+          imageSrcPath={imagePath}
+              navItems={items} />
+
       <h1>Home</h1>
       <p>Hello World! This is the Home page</p>
       <p>
