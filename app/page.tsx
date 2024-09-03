643ad3e74a470a7f0e77fc65334506e437f35936
ArmanAbadi <arman_abadi@hotmail.com>
2024-09-03 14:52:58 -0700

    idk man

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import NavBar from "./components/navbar";
+import NavBar from "./components/NavBar";
 
 const Home = () => {
     let items = ["Home", "Product", "Service"];
