ed5c5447e455c403ae14b873391c26bdb3c796b9
ArmanAbadi <arman_abadi@hotmail.com>
2024-09-03 16:14:07 -0700

    Reapply "new site lol"
    This reverts commit dfb58bb44978a33fdde0e25ac3c7d482f2ed70db.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,35 @@
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  // Control dark pseudo-selector by ancestor's "dark" class
+  darkMode: "class",
+  // Scan these files for Tailwind classes
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontSize: {
-      xs: '0.75rem',
-      sm: '0.875rem',
-      base: '1rem',
-      lg: '1.125rem',
-      xl: '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '4rem',
+    colors: {
+      // Necessary color utilities
+      transparent: colors.transparent,
+      current: colors.current,
+      // Primary accent color
+      primary: colors.blue,
+      // Grayscale
+      gray: colors.zinc,
+      // Gradient colors
+      neon: {
+        blue: colors.blue[500],
+        pink: colors.pink[500],
+        purple: colors.purple[500],
+        teal: colors.teal[400],
+        green: colors.green[500],
+        sky: colors.sky[500],
+        amber: colors.amber[500],
+        red: colors.red[500],
+      },
     },
     extend: {
-      colors: {
-        primary: {
-          100: '#E6F6FE',
-          200: '#C0EAFC',
-          300: '#9ADDFB',
-          400: '#4FC3F7',
-          500: '#03A9F4',
-          600: '#0398DC',
-          700: '#026592',
-          800: '#014C6E',
-          900: '#013349',
-        },
-        gray: {
-          100: '#f7fafc',
-          200: '#edf2f7',
-          300: '#e2e8f0',
-          400: '#cbd5e0',
-          500: '#a0aec0',
-          600: '#718096',
-          700: '#4a5568',
-          800: '#2d3748',
-          900: '#1a202c',
-        },
-      },
-      lineHeight: {
-        hero: '4.5rem',
+      fontFamily: {
+        sans: ["Avenir Next", "Helvetica Neue", "sans-serif"],
       },
     },
   },
