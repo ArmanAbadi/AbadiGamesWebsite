2880176892203a151aa707940caed48880e1d440
ArmanAbadi <arman_abadi@hotmail.com>
2024-09-04 04:00:43 -0700

    how

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/**/*.{html,js,svelte,ts}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -16,9 +15,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [ require('daisyui') ],
-  daisyui: {
-    themes: ["light", "dark"],
-  }
+  plugins: [],
 }
 export default config
