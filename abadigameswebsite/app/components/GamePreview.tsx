5b783919d2d51de7a8d10a27acb3eb88ed6adc00
ArmanAbadi <arman_abadi@hotmail.com>
2024-09-04 02:56:52 -0700

    Learning!!!

--- abadigameswebsite/app/components/GamePreview.tsx
+++ abadigameswebsite/app/components/GamePreview.tsx
@@ -5,7 +5,7 @@ import { UrlObject } from 'url'
 
 const GamePreview = (GamePreviewImageAndLinkProps: GamePreviewImageAndLinkProps) => {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card bg-base-100 w-96 shadow-xl"><Link href={GamePreviewImageAndLinkProps.GameLink}>
     <figure>
     <img
     src={String(GamePreviewImageAndLinkProps.ImageLink)}
@@ -13,10 +13,10 @@ const GamePreview = (GamePreviewImageAndLinkProps: GamePreviewImageAndLinkProps)
     </figure>
     <div className="card-body">
     <h2 className="card-title"> {GamePreviewImageAndLinkProps.GameName} </h2 >
-    <p><Link href={GamePreviewImageAndLinkProps.GameLink}>{GamePreviewImageAndLinkProps.GameDescription}</Link></p>
+    <p>{GamePreviewImageAndLinkProps.GameDescription}</p>
     <div className="card-actions justify-end">
     </div>
-    </div>
+    </div></Link>
     </div>
   )
 }
--- abadigameswebsite/app/page.tsx
+++ abadigameswebsite/app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import GamePreview from './components/GamePreview';
+import Navbar from './components/Navbar';
 
 const SlugsImageLink = '/Slugs.png';
 const SlugsGameLink = "https://armanabadi.github.io/Slugs/";
@@ -12,8 +13,24 @@ const SlugsGameDescription = "2D RPG with perlin noise map generation";
 export default function Home() {
   return (
     <main>
-      <h1>Hello world</h1>
+      <div className="navbar bg-base-100">
+        <a className="btn btn-ghost font-bold text-2xl">AbadiGames</a>
+      </div>
+
+      <div>
+        <p>  Hi, I'm Arman Abadi, I'm a full stack developer based out of Vancouver BC Canada. </p>
+        <p>  I have 7 years experience as a full stack developer.</p>
+        <p>  My main focus has been in the games industry but I have completed projects in a variety of fields!</p>
+      </div>
+
+      <div className="flex w-full flex-col">
+      <div className="divider"></div>
+      <div className="card bg-base-300 rounded-box grid place-items-center">
+
+
+      <div className = "flex justify-center">
       <GamePreview ImageLink={SlugsImageLink} GameLink={SlugsGameLink} GameName={SlugsGameTitle} GameDescription={SlugsGameDescription}></GamePreview>
+      </div></div></div>
     </main>
   )
 }
