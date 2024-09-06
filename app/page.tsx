07b4826b6f6f5e086bb444657625dfa28a6dd106
ArmanAbadi <arman_abadi@hotmail.com>
2024-09-06 02:48:21 -0700

    added email and linkedin links

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,14 @@ export default function Home() {
 
 <div className="navbar bg-base-100">
       <a className="btn btn-ghost font-bold text-2xl">AbadiGames</a>
-      <a className="btn glass" href="mailto:arman_abadi@hotmail.com?subject=Hi Arman, I'd like to hire you">Contact me!</a>
+      <div className="dropdown">
+  <div tabIndex={0} role="button" className="btn m-1">Contact me!</div>
+  <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-100 p-2 shadow">
+    <li><a href = "mailto:armanabadi.aa@gmail.com?subject=Hi Arman, I'd like to hire you">armanabadi.aa@gmail.com</a></li>
+    <li><a href = "https://www.linkedin.com/in/armanabadi/">https://www.linkedin.com/in/armanabadi/</a></li>
+  </ul>
 </div>
-
+</div>
 
 <div className="p-3 py-40 w-full bg-base-100">
       <div className="flex w-full justify-center text-2xl">
