09315e69e6eb3baeb2d1233972a2e4fe7991d866
ArmanAbadi <arman_abadi@hotmail.com>
2024-09-08 14:43:53 -0700

    set back to base to avoid dark jank

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   <div className="flex flex-col h-screen justify-between">
     <main className="flex flex-col h-screen justify-between">
 
-<div className="navbar p-3 bg-gray-100">
+<div className="navbar p-3 bg-base-100">
     <a className="btn btn-ghost font-bold text-2xl">AbadiGames</a>
 
     <div className=" flex flex-1 justify-end px-2">
@@ -32,7 +32,7 @@ export default function Home() {
 </div>
 
 
-<div className="p-3 py-32 w-full bg-gray-100">
+<div className="p-3 py-32 w-full bg-base-100">
       <div className="flex w-full justify-center text-2xl">
         <h1 className="text-center">  
           {"Hi, my name is Arman Abadi."}
