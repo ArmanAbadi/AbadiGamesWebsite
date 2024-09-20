6c69265a7d48477af6b9dcd2b51ccd1be0cc1a9c
ArmanAbadi <arman_abadi@hotmail.com>
2024-09-20 14:06:57 -0700

    addedpointer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -30,7 +31,7 @@ export default function Home() {
   </div>
 </div>
 
-<div className="container">
+<div className="container" onClick={() => DisplayDivNone()}>
             <svg viewBox="-10 0 120 120"><g className='hoverTarget'>
                 <defs>
                     <mask id="mask">
@@ -38,15 +39,17 @@ export default function Home() {
                     </mask>
                 </defs>
                 <image mask="url(#mask)"
-                    href="ArmanFace.png" x="-3" y="2" width="100" height="100">
+                    href="ArmanFace.png" x="-3" y="2" width="100" height="100" >
                 </image>
-                <g className="frame-border" fill="none">
+                <g className="frame-border" fill="none" >
                     <path id="Path_611" data-name="Path 611" d="M1,38a12.225,12.225,0,0,1,2.558-3.025L41.351,13.462A21.12,21.12,0,0,1,46.733,12.4a14.319,14.319,0,0,1,4.81.765L89.2,34.814A7.333,7.333,0,0,1,92,37a7.273,7.273,0,0,1,1,3.4v45.3A6.741,6.741,0,0,1,92,89a12.9,12.9,0,0,1-3.015,2.945L50.42,110.628a8.953,8.953,0,0,1-3.688.786,13.383,13.383,0,0,1-4.153-.992L4.2,92.012A12.105,12.105,0,0,1,1,89a7.112,7.112,0,0,1-1-3.581V41.534A9.569,9.569,0,0,1,1,38Z" transform="translate(1.502 -10.892)" stroke-linecap="round" />
                 </g></g>
             </svg>
         </div>
         
+        <div className='divNone' id="divNone">
 <Pond></Pond>
+</div>
 
 <div className="p-3 py-32 w-full font-extraboldTop">
       <div className="flex w-full justify-center">
@@ -114,3 +117,10 @@ export default function Home() {
   src="ArmanFace.png" />
   </div>
 </div> */}
+
+function DisplayDivNone() {
+  var T = document.getElementById("divNone");
+  if(T != null){
+    T.style.display = "block";  // <-- Set it to block
+  }
+}
