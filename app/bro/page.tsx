ff93b3d883fe998bb354152d91d1b80bc6a3e5b2
ArmanAbadi <arman_abadi@hotmail.com>
2024-10-08 15:10:26 -0700

    bruh

--- app/bro/page.tsx
+++ app/bro/page.tsx
@@ -20,7 +20,7 @@ export default function Home(){
     return(
         <div>
             <iframe 
-            src = "Battleboi/index.html" 
+            src = "BattleBoi/index.html" 
             width = "100%" 
             height = "100%" 
             style={{border:"none", position:"absolute", top:0, left: 0, right: 0, bottom: 0}}
